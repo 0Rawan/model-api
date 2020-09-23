2b43e7925aafbaf7058b2fc9e5724f866e7a69df
0rawan0 <49740553+0rawan0@users.noreply.github.com>
2020-09-23 22:34:15 +0300

    remove handle request method for now

--- app.py
+++ app.py
@@ -96,10 +96,8 @@ class Img (db.Model):
 ## 23 sep
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-    #if request.method == "POST":
-    #    and_json = request.files["text/plain"] 
-    #    return str(and_json)
-    return 'error'
+    and_json = request.files["text/plain"] 
+    return str(and_json)'
 ### meduim ahmad
 @app.route("/upload-image", methods=["GET", "POST"])
 def upload_image():
